6ac8cc3097b2ceb3dbe85b5dc55e6526a90f0e50
Olivia Lam <livilammister@gmail.com>
2019-10-23 14:56:53 -0500

    changed table to hold repository data

---
+++ src/app/irepository.ts
@@ -0,0 +1,6 @@
+export interface IRepository {
+    name: string,
+    numDownloads: number
+    numIssues: number,
+    numPullRequests: number
+}
--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
+import {IRepository} from '../irepository'
+
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -9,17 +11,11 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const repoData: IRepository[] = [
+  {name: 'Angular', numIssues: 1, numPullRequests: 2, numDownloads: 3},
+  {name: 'Ember', numIssues: 2, numPullRequests: 3, numDownloads: 4},
+  {name: 'React', numIssues: 3, numPullRequests: 4, numDownloads: 1},
+  {name: 'Vue', numIssues: 4, numPullRequests:1, numDownloads: 2},
 ];
 @Component({
   selector: 'app-product-table',
@@ -27,8 +23,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./product-table.component.sass']
 })
 export class ProductTableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['name', 'numDownloads', 'numIssues', 'numPullRequests'];
+  dataSource = new MatTableDataSource(repoData);
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
