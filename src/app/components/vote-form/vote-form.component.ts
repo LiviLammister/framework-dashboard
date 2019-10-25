434153f244130d7adb39f4e3fc569e03240319f0
Olivia Lam <livilammister@gmail.com>
2019-10-25 10:57:45 -0500

    added headers to post request

--- src/app/components/vote-form/vote-form.component.ts
+++ src/app/components/vote-form/vote-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-vote-form',
@@ -10,7 +11,10 @@ export class VoteFormComponent implements OnInit {
   isSubmitted: boolean = false;
   voteForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+    ) {
     this.createForm()
   }
 
@@ -24,7 +28,13 @@ export class VoteFormComponent implements OnInit {
   onSubmit() {
     this.isSubmitted = true
     if (this.voteForm.valid) {
-      console.log(JSON.stringify(this.voteForm.value))
+      const headers = new HttpHeaders().append("Content-Type", 'application/x-www-form-urlencoded;charset=UTF-8');
+      const { email, vote } = this.voteForm.value
+      this.http.post("http://localhost:3000/api/user", {email, vote}, { headers })
+        .subscribe(
+          res => console.log(res),
+          err => console.log(err)
+        )
     } else {
       return false
     }
