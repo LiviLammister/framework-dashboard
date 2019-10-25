b440ba8373ea28a762c741213dda821068b0effa
Olivia Lam <livilammister@gmail.com>
2019-10-24 19:51:09 -0500

    tables are bearable to look at now

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ResultService } from 'src/app/services/result.service';
+import { IResult } from 'src/app/interfaces/iresult';
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.sass']
 })
 export class ResultsComponent implements OnInit {
+  results: IResult[] = []
 
-  constructor() { }
+  constructor(private resultsService: ResultService) { }
 
   ngOnInit() {
+    this.results = this.resultsService.getResults()
   }
 
 }
