739a164bd667fccfa50db2098a95aaa9c2f7c2a2
Olivia Lam <livilammister@gmail.com>
2019-10-24 20:29:49 -0500

    form gathers data correctly.  Still needs validation

--- src/app/components/vote-form/vote-form.component.ts
+++ src/app/components/vote-form/vote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-vote-form',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vote-form.component.sass']
 })
 export class VoteFormComponent implements OnInit {
+  voteForm: FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.createForm()
+  }
+
+  createForm() {
+    this.voteForm = this.fb.group({
+      email: [''],
+      vote: ['']
+    })
+  }
+
+  onSubmit() {
+    console.log(JSON.stringify(this.voteForm.value))
+  }
 
   ngOnInit() {
   }
-
 }
